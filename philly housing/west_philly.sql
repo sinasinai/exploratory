f900862ebb87301a7d5295fa04d76f73fb2d4290
Sina Sinai <sina.sean.sinai@gmail.com>
2021-02-06 00:54:00 -0500

    update

--- philly housing/west_philly.sql
+++ philly housing/west_philly.sql
@@ -5,10 +5,16 @@ with l as (
 	from (
 			select
 			*,
-			row_number() over (partition by properties_address order by properties_mostrecentissuedate::timestamp desc) as rk
-			from licenses
-			where properties_address != 'nan'
-			and properties_licensetype = 'Rental'
+			row_number() over (partition by properties_address order by properties_mostrecentissuedate desc) as rk
+			from
+			(
+				select
+				*
+				from licenses
+				where properties_address != 'nan'
+				and properties_licensetype = 'Rental'
+				and properties_mostrecentissuedate != 'nan'
+			)
 		)
 		where rk = 1
 ),
@@ -22,9 +28,14 @@ p as (
 
 		select
 		*,
-		row_number() over (partition by properties_location order by properties_recording_date::timestamp desc) as rk
-		from properties
-
+		row_number() over (partition by properties_location order by properties_recording_date desc) as rk
+		from
+		(
+			select
+			*
+			from properties
+			where properties_recording_date != 'nan'
+		)
 	)
 
 	where rk = 1
@@ -71,7 +82,7 @@ properties_number_stories as stories,
 properties_owner_1 as owner_1,
 properties_owner_2 as owner_2,
 properties_parcel_number as parcel_number,
-properties_recording_date::timestamp as property_recording_date,
+properties_recording_date,
 properties_sale_price as sale_price,
 properties_taxable_building as taxable_building,
 properties_taxable_land as taxable_land,
@@ -80,6 +91,6 @@ properties_total_livable_area as total_livable_area,
 properties_zip_code as zip_code,
 properties_zoning as zoning
 from combined
-where left(properties_zip,5)
-in ('19134', '19144', '19139', '19131', '19154', '19120', '19124', '19141', '19143', '19104')
+where left(properties_zip,5) in ('19134', '19144', '19139', '19131', '19154', '19120', '19124', '19141', '19143', '19104')
+and properties_zip != 'nan'
 order by properties_zip, properties_address
