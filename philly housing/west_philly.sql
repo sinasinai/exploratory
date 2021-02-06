7cf91e002192146be89f2488ac25991848863e5c
Sina Sinai <sina.sean.sinai@gmail.com>
2021-02-06 00:39:25 -0500

    adding philly housing

---
+++ philly housing/west_philly.sql
@@ -0,0 +1,85 @@
+with l as (
+
+	select
+	*
+	from (
+			select
+			*,
+			row_number() over (partition by properties_address order by properties_mostrecentissuedate::timestamp desc) as rk
+			from licenses
+			where properties_address != 'nan'
+			and properties_licensetype = 'Rental'
+		)
+		where rk = 1
+),
+
+
+p as (
+
+	select
+	*
+	from (
+
+		select
+		*,
+		row_number() over (partition by properties_location order by properties_recording_date::timestamp desc) as rk
+		from properties
+
+	)
+
+	where rk = 1
+
+
+),
+
+combined as (
+
+
+	select
+	*
+	from l
+	inner join properties p on l.properties_address = p.properties_location and left(p.properties_zip_code::varchar,5) = left(l.properties_zip::varchar,5)
+
+)
+
+
+
+
+select
+properties_address,
+properties_market_value,
+properties_zip,
+properties_opa_account_num,
+properties_opa_owner,
+properties_licensenum as properties_rental_license_number,
+properties_mostrecentissuedate as most_recent_license_issue_date,
+properties_expirationdate as license_expiration_date,
+properties_inactivedate as license_inactive_date,
+properties_licensestatus as license_status,
+properties_numberofunits as number_of_units,
+properties_legalname as legal_property_owner_name,
+properties_legalentitytype as legal_entity_type,
+properties_business_name as business_name,
+properties_business_mailing_address as owner_business_mailing_address,
+properties_building_code_description as building_code,
+properties_category_code_description as category_code,
+properties_census_tract as census_tract,
+properties_number_of_bedrooms as bedrooms,
+properties_number_of_bathrooms as bathrooms,
+properties_number_of_rooms as rooms,
+properties_number_stories as stories,
+properties_owner_1 as owner_1,
+properties_owner_2 as owner_2,
+properties_parcel_number as parcel_number,
+properties_recording_date::timestamp as property_recording_date,
+properties_sale_price as sale_price,
+properties_taxable_building as taxable_building,
+properties_taxable_land as taxable_land,
+properties_total_area as total_area,
+properties_total_livable_area as total_livable_area,
+properties_zip_code as zip_code,
+properties_zoning as zoning
+from combined
+where left(properties_zip,5)
+in ('19134', '19144', '19139', '19131', '19154', '19120', '19124', '19141', '19143', '19104')
+order by properties_zip, properties_address
